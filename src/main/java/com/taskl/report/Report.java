e43c5143088ac8176349e0bd9c18fb37e7b4e595
Lukasz Biernacki <lbiernacki@student.agh.edu.pl>
2024-06-08 15:50:21 +0200

    Report class test

--- src/main/java/com/taskl/report/Report.java
+++ src/main/java/com/taskl/report/Report.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 public class Report {
 
+
     public static void printReportALL(List<Project> projectList) {
         System.out.println("---------Report for all projects---------");
         for (Project project : projectList) {
