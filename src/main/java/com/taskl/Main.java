464b3d34fcfc444973e99b37bc4f3e04d976e2a9
Aroo-dev <axxrxxo@gmail.com>
2024-06-08 13:25:00 +0200

    stworzenie prowizorycznej bazy danych, z której wyciągniemy liste projektów

--- src/main/java/com/taskl/Main.java
+++ src/main/java/com/taskl/Main.java
@@ -1,7 +1,11 @@
 package com.taskl;
 
+import com.taskl.db.DataBase;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        DataBase dataBase = new DataBase();
+        dataBase.importProject("Dupa");
+
     }
 }
