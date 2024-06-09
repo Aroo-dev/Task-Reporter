7824fbf459f3f5bc2a6908ee012d81956c99b49d
aroo-dev <axxrxoo@gmail.com>
2024-06-09 13:58:20 +0200

    implemented stop function and test

--- src/main/java/com/task/functions/StopTask.java
+++ src/main/java/com/task/functions/StopTask.java
@@ -3,11 +3,37 @@ package com.task.functions;
 import com.task.db.DataBase;
 import com.task.model.Project;
 import com.task.model.Task;
+import com.task.reader.Reader;
+import com.task.writer.Writer;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 
+import java.io.File;
+import java.io.IOException;
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 public class StopTask {
 
+    public static void stopTask() throws IOException, InvalidFormatException {
+        File file = new File("file.xlsx");
+
+        Task lastTask = Reader.getLast(file);
+        if (lastTask == null){
+            throw  new RuntimeException("There's no task to be closed");
+        }if(lastTask.isClosed()){
+            System.out.println("The current task is already stopped");
+        }else {
+            lastTask.setStopTask(LocalDateTime.now());
+            lastTask.setIsClosed(true);
+            Duration duration = Duration.between(lastTask.getStartTask(),lastTask.getStopTask());
+            lastTask.setTotalTime(duration);
+            List<Task> lastTasks = List.of(lastTask);
+            Writer.overWriteLastRows(lastTasks,file);
+
+        }
+    }
 
 }
 
